fc66e134154e87c0d181fbfe9973d5f0235cf6af
NGUYEN VIET DUC <1621050485@student.humg.edu.vn>
2021-01-18 16:29:19 +0700

    Add project files.

---
+++ image/image/Page1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace image
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public int imageID =1;
+        public Page1()
+        {
+            InitializeComponent();
+            imageID = 1;
+            loadimage();
+        }
+        void loadimage()
+        {
+           active.IsVisible = true;
+            if(imageID == 1)
+                image.Source = "a.png";
+            else if (imageID == 2)
+                image.Source = "b";
+            else
+                image.Source = "c";
+            active.IsVisible = false;
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            imageID--;
+            if (imageID == 0)
+                imageID = 3;
+            loadimage();
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            imageID++;
+            if (imageID == 4)
+                imageID = 1;
+            loadimage();
+        }
+    }
+}
